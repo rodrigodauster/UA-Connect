d63db097eeb7f324145f93f4131e53cfb6318548
Rodrigo Dauster <rdauster@gmail.com>
2026-07-07 23:00:03 +0200

    Initial Commit

---
+++ UA Connect/UA_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UA_ConnectApp.swift
+//  UA Connect
+//
+//  Created by Rodrigo Dauster on 07/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct UA_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UA ConnectTests/UA_ConnectTests.swift
@@ -0,0 +1,18 @@
+//
+//  UA_ConnectTests.swift
+//  UA ConnectTests
+//
+//  Created by Rodrigo Dauster on 07/07/2026.
+//
+
+import Testing
+
+struct UA_ConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
